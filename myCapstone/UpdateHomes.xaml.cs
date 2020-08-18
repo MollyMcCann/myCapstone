620191c6dc08ac798679f7b166781400e1ecf545
Molly Mccann <mollymccann@bellevuecollege.edu>
2020-08-17 17:15:50 -0700

    .

--- myCapstone/UpdateHomes.xaml.cs
+++ myCapstone/UpdateHomes.xaml.cs
@@ -59,7 +59,7 @@ namespace myCapstone
 
                 realEstateCompaniesCollection = new RealEstateCompanyCollection(db.RealEstateCompanies.ToList());
                 this.CompanyListBox.DisplayMemberPath = "CompanyName";
-                this.CompanyListBox.SelectedValuePath = "CompanyId";
+                this.CompanyListBox.SelectedValuePath = "CompanyID";
                 this.CompanyListBox.ItemsSource = realEstateCompaniesCollection;
 
 
@@ -70,106 +70,120 @@ namespace myCapstone
         {
             if (CheckAgent.IsChecked.HasValue && (bool)CheckAgent.IsChecked)
             {
-                Person personObject = new Person();
-                HomeTrackerDatamodelLibrary.Agent agentObject = new HomeTrackerDatamodelLibrary.Agent();
-                personObject.FirstName = firstName.Text;
-                personObject.LastName = lastName.Text;
-                personObject.Phone = phoneNumber.Text;
-                personObject.Email = email.Text;
-                decimal commPerc;
-
-                if (!decimal.TryParse(comissionPercentage.Text, out commPerc))
+                try
                 {
-                    // TODO: Notify user of failure
-                    return;
-                }
+                    Person personObject = new Person();
+                    HomeTrackerDatamodelLibrary.Agent agentObject = new HomeTrackerDatamodelLibrary.Agent();
+                    personObject.FirstName = firstName.Text;
+                    personObject.LastName = lastName.Text;
+                    personObject.Phone = phoneNumber.Text;
+                    personObject.Email = email.Text;
+                    decimal commPerc;
 
-                agentObject.CommissionPercent = commPerc;
-                //todo: 1. add agent company to UI, listbox
-                //2. use a similar pattern to the house list box. get the DisplayValuePath will be Company Name
-                //    DisplayValuePath will be CompanyID
-                //3. will be similar to the comment below:
-                //if (HomeListBox.SelectedIndex == -1)
-                //{
-                //    return;
-                //    //todo tell user we cannot add the home
-                //}
-                //agentObject.CompanyID = (int)HomeListBox.SelectedValue;
-                if (CompanyListBox.SelectedIndex == -1)
+                    if (!decimal.TryParse(comissionPercentage.Text, out commPerc))
+                    {
+                        // TODO: Notify user of failure
+                        return;
+                    }
+
+                    agentObject.CommissionPercent = commPerc;
+                    //todo: 1. add agent company to UI, listbox
+                    //2. use a similar pattern to the house list box. get the DisplayValuePath will be Company Name
+                    //    DisplayValuePath will be CompanyID
+                    //3. will be similar to the comment below:
+                    //if (HomeListBox.SelectedIndex == -1)
+                    //{
+                    //    return;
+                    //    //todo tell user we cannot add the home
+                    //}
+                    //agentObject.CompanyID = (int)HomeListBox.SelectedValue;
+                    if (CompanyListBox.SelectedIndex == -1)
+                    {
+                        return;
+
+                    }
+                    agentObject.CompanyID = (int)CompanyListBox.SelectedValue;// is returning a null value...why?
+
+
+                    personObject.Agent = agentObject;
+                    peopleUd.Add(personObject);
+
+                    HomeSale homeSalesObject = new HomeSale();
+                    if (HomeListBox.SelectedIndex == -1)
+                    {
+                        return;
+                        //todo tell user we cannot add the home
+                    }
+                    homeSalesObject.HomeID = (int)HomeListBox.SelectedValue;
+                    homeSalesObject.AgentID = personObject.Agent.AgentID;
+                    homeSalesObject.MarketDate = DateTime.Now;
+                    //homeSalesObject.CompanyID = personObject.Agent.CompanyID; 
+                    homeSalesObject.CompanyID = (int)CompanyListBox.SelectedValue;//fix this
+                    decimal SAmount;
+
+                    if (!decimal.TryParse(SaleAmount.Text, out SAmount))
+                    {
+                        // TODO: Notify user of failure
+                        return;
+                    }
+
+
+                    homeSalesObject.SaleAmount = SAmount;
+
+                    homeSalesCollection.Add(homeSalesObject);
+                }
+                catch(Exception ex)
                 {
-                    return;
-                   
+                 
                 }
-                agentObject.CompanyID = (int)CompanyListBox.SelectedValue;// is returning a null value...why?
-
-
-                personObject.Agent = agentObject;
-                peopleUd.Add(personObject);
-
-                HomeSale homeSalesObject = new HomeSale();
-                if(HomeListBox.SelectedIndex == -1)
-                {
-                    return;
-                    //todo tell user we cannot add the home
-                }
-                homeSalesObject.HomeID = (int)HomeListBox.SelectedValue;
-                homeSalesObject.AgentID = personObject.Agent.AgentID;
-                homeSalesObject.MarketDate = DateTime.Now;
-                //homeSalesObject.CompanyID = personObject.Agent.CompanyID; 
-                homeSalesObject.CompanyID = CompanyListBox.SelectedIndex;//fix this
-                decimal SAmount;
-
-                if (!decimal.TryParse(SaleAmount.Text, out SAmount))
-                {
-                    // TODO: Notify user of failure
-                    return;
-                }
-
-               
-                homeSalesObject.SaleAmount = SAmount;
-
-                homeSalesCollection.Add(homeSalesObject);
             }
             else if(CheckBuyer.IsChecked.HasValue && (bool)CheckBuyer.IsChecked)
             {
-                Person personObject = new Person();
-                HomeTrackerDatamodelLibrary.Buyer buyerObject = new HomeTrackerDatamodelLibrary.Buyer();
-                personObject.FirstName = firstNameB.Text;
-                personObject.LastName = lastNameB.Text;
-                personObject.Phone = phoneNumberB.Text;
-                personObject.Email = emailB.Text;
-                int credRate;
-                if(! int.TryParse(creditRatingB.Text, out credRate))
+                try
                 {
-                    return;
+
+                    Person personObject = new Person();
+                    HomeTrackerDatamodelLibrary.Buyer buyerObject = new HomeTrackerDatamodelLibrary.Buyer();
+                    personObject.FirstName = firstNameB.Text;
+                    personObject.LastName = lastNameB.Text;
+                    personObject.Phone = phoneNumberB.Text;
+                    personObject.Email = emailB.Text;
+                    int credRate;
+                    if (!int.TryParse(creditRatingB.Text, out credRate))
+                    {
+                        return;
+                    }
+                    buyerObject.CreditRating = credRate;
+
+                    personObject.Buyer = buyerObject;
+                    peopleUd.Add(personObject);
+
+                    HomeSale homeSalesObject = new HomeSale();
+                    if (HomeListBox.SelectedIndex == -1)
+                    {
+                        return;
+                        //todo tell user we cannot add the home
+                    }
+
+                    homeSalesObject.HomeID = (int)HomeListBox.SelectedValue;
+                    homeSalesObject.SoldDate = DateTime.Now;
+                    decimal SAmount1;
+
+                    if (!decimal.TryParse(SoldAmount.Text, out SAmount1))
+                    {
+                        // TODO: Notify user of failure
+                        return;
+                    }
+                    homeSalesObject.SaleAmount = SAmount1;
+                    homeSalesObject.BuyerID = personObject.Buyer.BuyerID;
+                    homeSalesObject.CompanyID = CompanyListBox.SelectedIndex;//fix this
+                    homeSalesCollection.Add(homeSalesObject);
+
                 }
-                buyerObject.CreditRating = credRate;
-
-                personObject.Buyer = buyerObject;
-                peopleUd.Add(personObject);
-
-                HomeSale homeSalesObject = new HomeSale();
-                if (HomeListBox.SelectedIndex == -1)
+                catch(Exception ex)
                 {
-                    return;
-                    //todo tell user we cannot add the home
+
                 }
-                
-                homeSalesObject.HomeID = (int)HomeListBox.SelectedValue;
-                homeSalesObject.SoldDate = DateTime.Now;
-                decimal SAmount;
-
-                if (!decimal.TryParse(SaleAmount.Text, out SAmount))
-                {
-                    // TODO: Notify user of failure
-                    return;
-                }
-                homeSalesObject.SaleAmount = SAmount;
-                homeSalesObject.BuyerID = personObject.Buyer.BuyerID;
-                homeSalesObject.CompanyID = CompanyListBox.SelectedIndex;//fix this
-                homeSalesCollection.Add(homeSalesObject);
-
-
 
             }
 
