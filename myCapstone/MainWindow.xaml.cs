c9aa3ee0605023b0979b11cf769cdd9959f7820e
Molly Mccann <mollymccann@bellevuecollege.edu>
2020-08-19 16:57:10 -0700

    finishing up the datagrid

--- myCapstone/MainWindow.xaml.cs
+++ myCapstone/MainWindow.xaml.cs
@@ -138,19 +138,130 @@ namespace myCapstone
 
 
 
-        private void Button_Click_5(object sender, RoutedEventArgs e)
+        private void Button_Click_5(object sender, RoutedEventArgs e)//home sales
         {
             HomeDataGrid.DataContext = homeSalesCollection;
+            HomeDataGrid.Columns.Clear();
+
+            DataGridTextColumn saleIDColumn = new DataGridTextColumn
+            {
+                Header = "Sale ID",
+                Binding = new Binding("SaleID")
+            };
+            HomeDataGrid.Columns.Add(saleIDColumn);
+            DataGridTextColumn HomeIDColumn = new DataGridTextColumn
+            {
+                Header = "Home Id",
+                Binding = new Binding("HomeID")
+            };
+            HomeDataGrid.Columns.Add(HomeIDColumn);
+            DataGridTextColumn soldDateColumn = new DataGridTextColumn
+            {
+                Header = "Sold Date",
+                Binding = new Binding("SoldDate")
+
+            };
+            HomeDataGrid.Columns.Add(soldDateColumn);
+            DataGridTextColumn AgentIDColumn = new DataGridTextColumn
+            {
+                Header = "Agent ID",
+                Binding = new Binding("AgentID")
+
+            };
+            HomeDataGrid.Columns.Add(AgentIDColumn);
+           
+            
+            DataGridTextColumn saleAmountColumn = new DataGridTextColumn
+            {
+                Header = "Sale Amount",
+                Binding = new Binding("SaleAmount")
+
+            };
+            HomeDataGrid.Columns.Add(saleAmountColumn);
+            DataGridTextColumn buyerID1Column = new DataGridTextColumn
+            {
+                Header = "Buyer ID",
+                Binding = new Binding("BuyerID")
+
+            };
+            HomeDataGrid.Columns.Add(buyerID1Column);
+            DataGridTextColumn marketDateColumn = new DataGridTextColumn
+            {
+                Header = "Market Date",
+                Binding = new Binding("MarketDate")
+
+            };
+            HomeDataGrid.Columns.Add(marketDateColumn);
+            DataGridTextColumn CompanyIDColumn = new DataGridTextColumn
+            {
+                Header = "Company ID",
+                Binding = new Binding("CompanyID")
+
+            };
+            HomeDataGrid.Columns.Add(CompanyIDColumn);
+
+
         }
 
         private void Button_Click_6(object sender, RoutedEventArgs e)
         {
             HomeDataGrid.DataContext = realEstateCompaniesCollection;
+            HomeDataGrid.Columns.Clear();
+            DataGridTextColumn companyIDColumn = new DataGridTextColumn
+            {
+                Header = "Company ID",
+                Binding = new Binding("CompanyID")
+            };
+            HomeDataGrid.Columns.Add(companyIDColumn);
+            DataGridTextColumn companyNameColumn = new DataGridTextColumn
+            {
+                Header = "Company Name",
+                Binding = new Binding("CompanyName")
+            };
+            HomeDataGrid.Columns.Add(companyNameColumn);
+            DataGridTextColumn phoneNumberColumn = new DataGridTextColumn
+            {
+                Header = "Phone Number",
+                Binding = new Binding("Phone")
+            };
+            HomeDataGrid.Columns.Add(phoneNumberColumn);
         }
 
         private void Button_Click_7(object sender, RoutedEventArgs e)
         {
             HomeDataGrid.DataContext = homeCollection;
+            HomeDataGrid.Columns.Clear();
+            DataGridTextColumn homeIDColumn = new DataGridTextColumn
+            {
+                Header = "Home ID",
+                Binding = new Binding("HomeID")
+            };
+            HomeDataGrid.Columns.Add(homeIDColumn);
+            DataGridTextColumn addressColumn = new DataGridTextColumn
+            {
+                Header = "Address",
+                Binding = new Binding("Address")
+            };
+            HomeDataGrid.Columns.Add(addressColumn);
+            DataGridTextColumn cityColumn = new DataGridTextColumn
+            {
+                Header = "City",
+                Binding = new Binding("City")
+            };
+            HomeDataGrid.Columns.Add(cityColumn);
+            DataGridTextColumn stateColumn = new DataGridTextColumn
+            {
+                Header = "State",
+                Binding = new Binding("State")
+            };
+            HomeDataGrid.Columns.Add(stateColumn);
+            DataGridTextColumn zipColumn = new DataGridTextColumn
+            {
+                Header = "Zip Code",
+                Binding = new Binding("Zip")
+            };
+            HomeDataGrid.Columns.Add(zipColumn);
         }
+
     }
 }
